fab21740e7f14c3f4e04280b2dba4c324232e108
Yuki Iwanaga <yuki@creasty.com>
2015-06-07 02:42:39 +0900

    Introduce realm

---
+++ Cerberus/Classes/Models/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  Cerberus
+//
+//  Created by Yuki Iwanaga on 6/7/15.
+//  Copyright (c) 2015 Wantedly, Inc. All rights reserved.
+//
+
+import Foundation
--- Podfile
+++ Podfile
@@ -13,3 +13,4 @@ inhibit_all_warnings!
 
 pod 'Timepiece'
 pod 'Async', :git => 'https://github.com/duemunk/Async.git'
+pod 'Realm'
