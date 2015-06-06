e50f57bdc875cd39b1fec826c9cf7947ef14b72f
Kento Moriwaki <kento@wantedly.com>
2015-06-07 02:07:38 +0900

    Set border styles

--- Cerberus/Classes/EventCollectionViewCell.swift
+++ Cerberus/Classes/EventCollectionViewCell.swift
@@ -6,6 +6,7 @@ class EventCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var attendeesLabel: UILabel!
+    @IBOutlet weak var wrapView: UIView!
 
     var eventModel: Event? {
         didSet { update() }
@@ -18,5 +19,6 @@ class EventCollectionViewCell: UICollectionViewCell {
             self.titleLabel.text = event.title
             self.timeLabel.text  = time
         }
+        wrapView.layer.borderColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.3).CGColor
     }
 }
