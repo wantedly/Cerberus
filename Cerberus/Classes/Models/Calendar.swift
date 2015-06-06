751a9b7a2a494275a142fe389c6406f836711290
Yuki Iwanaga <yuki@creasty.com>
2015-06-06 14:13:04 +0900

    Create models

---
+++ Cerberus/Classes/Models/Calendar.swift
@@ -0,0 +1,13 @@
+//
+//  Calendar.swift
+//  Cerberus
+//
+//  Created by Yuki Iwanaga on 6/6/15.
+//  Copyright (c) 2015 Wantedly, Inc. All rights reserved.
+//
+
+import Foundation
+
+final class Calendar {
+  var events: [Event] = []
+}
---
+++ Cerberus/Classes/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Cerberus
+//
+//  Created by Yuki Iwanaga on 6/6/15.
+//  Copyright (c) 2015 Wantedly, Inc. All rights reserved.
+//
+
+import Foundation
+
+class User {
+  var name: String = ""
+  var email: String = ""
+  
+  func avatarUrl() -> String {
+    return ""
+  }
+}
