eedfa418372e3c3787e82dd93f90061f7552003b
Yuki Iwanaga <yuki@creasty.com>
2015-06-06 17:01:25 +0900

    Omit header comments

--- Cerberus/Classes/Models/Calendar.swift
+++ Cerberus/Classes/Models/Calendar.swift
@@ -1,11 +1,3 @@
-//
-//  Calendar.swift
-//  Cerberus
-//
-//  Created by Yuki Iwanaga on 6/6/15.
-//  Copyright (c) 2015 Wantedly, Inc. All rights reserved.
-//
-
 import Foundation
 import EventKit
 
--- Cerberus/Classes/Models/Event.swift
+++ Cerberus/Classes/Models/Event.swift
@@ -1,11 +1,3 @@
-//
-//  Event.swift
-//  Cerberus
-//
-//  Created by Yuki Iwanaga on 6/6/15.
-//  Copyright (c) 2015 Wantedly, Inc. All rights reserved.
-//
-
 import Foundation
 
 final class Event {
