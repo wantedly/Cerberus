cdf1c8366005c62955c98109683f59f4f125fef9
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2015-06-06 18:22:08 +0900

    Add setting height logic

--- Cerberus/Classes/EventsCollectionViewController.swift
+++ Cerberus/Classes/EventsCollectionViewController.swift
@@ -4,7 +4,9 @@ import Async
 class EventsCollectionViewController: UICollectionViewController {
 
     var calendar: Calendar!
-    
+
+    var arr: [[Int]] = [[8*60, 9*60], [11*60+30, 13*60]]
+
     let reuseIdentifier = "EventCell"
 
     override func viewDidLoad() {
@@ -43,5 +45,30 @@ class EventsCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! EventCollectionViewCell
         cell.titleLabel.text = self.calendar.events[indexPath.row].title
         return cell
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let minuteHeight: CGFloat = 2;
+        let width: CGFloat = collectionView.bounds.width
+        let height: CGFloat = minuteHeight * CGFloat(createCalendarArray(arr)[indexPath.item])
+
+        return CGSizeMake(width, height)
+    }
+
+    func createCalendarArray(array: [[Int]]) -> [Int] {
+        var calendarArr: [Int] = []
+        var cur = 0;
+        for start_end in array {
+            let start = start_end[0], end = start_end[1]
+            if cur < start {
+                calendarArr.append(start - cur)
+            }
+            calendarArr.append(end - start)
+            cur = end
+        }
+        if cur < 24 * 60 {
+            calendarArr.append(24 * 60 - cur)
+        }
+        return calendarArr
     }
 }
