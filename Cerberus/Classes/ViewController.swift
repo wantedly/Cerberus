d622d31dd2270c75dfb513b15cbb3621b110e1fb
Jiro Nagashima <hedjirog@gmail.com>
2015-06-06 11:45:55 +0900

    Add project files

---
+++ Cerberus/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cerberus
+//
+//  Created by Jiro Nagashima on 2015/06/06.
+//  Copyright (c) 2015年 Wantedly, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
