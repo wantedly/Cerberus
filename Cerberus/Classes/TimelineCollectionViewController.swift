ae65efad402f0accce77d2b8a798b6148ecc0120
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2015-06-06 15:21:26 +0900

    Move position to current time

--- Cerberus/Classes/TimelineCollectionViewController.swift
+++ Cerberus/Classes/TimelineCollectionViewController.swift
@@ -13,6 +13,17 @@ class TimelineCollectionViewController: UICollectionViewController {
         }
     }
 
+    override func viewDidAppear(animated: Bool) {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: date)
+        let hour = components.hour
+        let minute = components.minute
+
+        let newIndex = NSIndexPath(forItem: (hour * 60 + minute) / 30, inSection: 0)
+        self.collectionView?.scrollToItemAtIndexPath(newIndex, atScrollPosition: UICollectionViewScrollPosition.CenteredVertically, animated: true)
+    }
+
     // MARK: UICollectionViewDataSource
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
