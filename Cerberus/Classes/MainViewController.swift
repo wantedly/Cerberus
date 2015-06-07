c683a18fd38b79bf8ba3443da9dc9e39a37a45d3
Jiro Nagashima <hedjirog@gmail.com>
2015-06-07 19:51:27 +0900

    Sync scrollings

--- Cerberus/Classes/MainViewController.swift
+++ Cerberus/Classes/MainViewController.swift
@@ -4,7 +4,30 @@ import EventKitUI
 
 class MainViewController: UIViewController, EKCalendarChooserDelegate {
 
+    weak var timelineCollectionViewController: TimelineCollectionViewController?
+    weak var eventsCollectionViewController: EventsCollectionViewController?
     var calendarChooser: EKCalendarChooser!
+
+    var KVOContext = "MainViewControllerKVOContext"
+    let contentOffsetKeyPath = "contentOffset"
+
+    deinit {
+        self.timelineCollectionViewController?.removeObserver(self, forKeyPath: contentOffsetKeyPath)
+        self.eventsCollectionViewController?.removeObserver(self, forKeyPath: contentOffsetKeyPath)
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        switch segue.destinationViewController {
+        case let timelineCollectionViewController as TimelineCollectionViewController:
+            self.timelineCollectionViewController = timelineCollectionViewController
+            self.timelineCollectionViewController?.collectionView?.addObserver(self, forKeyPath: contentOffsetKeyPath, options: .New, context: &KVOContext)
+        case let eventsCollectionViewController as EventsCollectionViewController:
+            self.eventsCollectionViewController = eventsCollectionViewController
+            self.eventsCollectionViewController?.collectionView?.addObserver(self, forKeyPath: contentOffsetKeyPath, options: .New, context: &KVOContext)
+        default:
+            break
+        }
+    }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -49,5 +72,26 @@ class MainViewController: UIViewController, EKCalendarChooserDelegate {
         NSNotificationCenter.defaultCenter().postNotificationName(NotifictionNames.MainViewControllerDidChooseCalendarNotification.rawValue, object: calendars)
 
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // MARK: Key Value Observing
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if context == &KVOContext && keyPath == contentOffsetKeyPath {
+            if let collectionView = object as? UICollectionView {
+                var anotherCollectionViewController: UICollectionViewController?
+                if collectionView == timelineCollectionViewController?.collectionView {
+                    anotherCollectionViewController = eventsCollectionViewController
+                } else if collectionView == eventsCollectionViewController?.collectionView {
+                    anotherCollectionViewController = timelineCollectionViewController
+                }
+
+                if let anotherCollectionView = anotherCollectionViewController?.collectionView {
+                    if anotherCollectionView.contentOffset.y != collectionView.contentOffset.y {
+                        anotherCollectionView.contentOffset = collectionView.contentOffset
+                    }
+                }
+            }
+        }
     }
 }
